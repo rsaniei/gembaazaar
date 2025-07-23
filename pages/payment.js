dc386abae814aae4566bfc3e3d8051505d9ce416
rsaniei <rs_ir2005@yahoo.com>
2025-07-23 14:43:14 +0200

    save data in cookie

--- pages/payment.js
+++ pages/payment.js
@@ -1,14 +1,37 @@
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import { useState, useContext } from "react";
 import Layout from "@/components/Layout";
 import CheckoutWizard from "@/components/CheckoutWizard";
-import { useRouter } from "next/router";
+import { CartContext } from "@/context/Cart";
 
 export default function PaymentPage() {
+	const { state, dispatch } = useContext(CartContext);
+	const { cart } = state;
+	const { paymentMethod } = cart;
+	const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(
+		paymentMethod || ""
+	);
+	console.log(selectedPaymentMethod);
+
 	const router = useRouter();
+
 	const methods = ["Gateway", "Offline Payment"];
+
+	function submitHandler(event) {
+		event.preventDefault();
+		if (!selectedPaymentMethod) alert("Please select a payment method");
+		dispatch({ type: "SAVE_PAYMENT_METHOD", payload: selectedPaymentMethod });
+		Cookies.set(
+			"cart",
+			JSON.stringify({ ...cart, paymentMethod: selectedPaymentMethod })
+		);
+		router.push("/placeOrder");
+	}
 	return (
 		<Layout title="Payment Page">
 			<CheckoutWizard activeStep={2} />
-			<form className="mx-auto max-w-screen-md">
+			<form className="mx-auto max-w-screen-md" onSubmit={submitHandler}>
 				<h2 className="mb-4 text-xl">Payment Method:</h2>
 				{methods.map((item) => (
 					<div key={item} className="mb-4">
@@ -17,6 +40,8 @@ export default function PaymentPage() {
 							name="paymentMethod"
 							className="p-2"
 							id={item}
+							checked={selectedPaymentMethod === item}
+							onChange={() => setSelectedPaymentMethod(item)}
 						/>
 						<label className="p-2" htmlFor={item}>
 							{item}
