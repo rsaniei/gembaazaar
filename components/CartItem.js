4ba36328b2a46c19949209809880cceaf2aacd5b
rsaniei <rs_ir2005@yahoo.com>
2025-08-05 15:51:26 +0200

    show multiple images in the product page

--- components/CartItem.js
+++ components/CartItem.js
@@ -11,14 +11,23 @@ export default function CartItem({ item }) {
 	}
 	return (
 		<div className="bg-white flex flex-col md:flex-row gap-10 p-10">
-			<Image src={item.image} alt={item.title} width={140} height={140}></Image>
+			<Image
+				src={item.image[0]}
+				alt={item.title}
+				width={140}
+				height={140}
+			></Image>
 			<div className="flex flex-col col-span-2">
-				<h2>{item.title}</h2>
+				<h2 className="text-md">{item.title}</h2>
 				<div>{item.price}</div>
-				<div>{item.description}</div>
 				<div>{item.qty}</div>
 				<div>
-					<button onClick={() => removeItemHandler(item)}>Remove</button>
+					<button
+						className="bg-white border border-black text-black text-sm  w-40 px-3 py-3 mt-5 cursor-pointer"
+						onClick={() => removeItemHandler(item)}
+					>
+						Remove
+					</button>
 				</div>
 			</div>
 		</div>
