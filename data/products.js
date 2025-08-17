618dc54f9d43a4126d4802134af1883411fe360c
rsaniei <rs_ir2005@yahoo.com>
2025-08-17 22:48:27 +0200

    add extra products

--- data/products.js
+++ data/products.js
@@ -5,9 +5,29 @@ const products = [
 		slug: "10K-Gold-Statement-Clip-On-Earrings",
 		description:
 			"A striking pair of vintage 10K gold clip-on earrings, featuring a bold geometric design with a hammered texture and beaded edges. Measuring 2.5 inches long and weighing 44 grams combined, these statement earrings exude elegance and timeless charm. Year 1980s",
-		category: "Earrings",
+		category: "earrings",
 		count: 5,
 		image: ["/images/e10.webp", "/images/e11.webp", "/images/e12.webp"],
+	},
+	{
+		title: "Antique Silver-Tone Bracelet with Blue Glass and Faux Pearls",
+		price: 216.95,
+		slug: "antique-silver-tone-bracelet-with-blue-glass-and-faux-pearls",
+		description:
+			"This antique silver-tone bracelet features five ornate panels, each adorned with a deep blue glass centerpiece framed by delicate faux pearls and accented with clear rhinestones. The intricate openwork design and secure clasp add to its timeless appeal. Measuring 8 inches long and 2 inches wide, this statement piece weighs 100g and evokes Victorian-inspired elegance, likely from the 1950s.",
+		category: "bracelet",
+		count: 5,
+		image: ["/images/b11.webp", "/images/b10.webp", "/images/b12.webp"],
+	},
+	{
+		title: "Anne Klein Matte Gold Collar Necklace",
+		price: 108.95,
+		slug: "anne-klein-matte-gold-collar-necklace",
+		description:
+			"Matte gold-tone collar necklace featuring domed circular links with a bold, sculptural design. Signed Anne Klein on the tag, this gold-plated piece has minor gold dusting consistent with age. It weighs 100g, measures 17.5 inches long, and is 1.2 inches wide. From the 1980s.",
+		category: "necklace",
+		count: 5,
+		image: ["/images/n10.webp", "/images/n11.webp", "/images/n12.webp"],
 	},
 	{
 		title: "1980s Valentino 10K Clip-On Earrings",
@@ -15,7 +35,7 @@ const products = [
 		slug: "1980s-Valentino-10K-Clip-On-Earrings",
 		description:
 			"These vintage 1980s Valentino clip-on earrings feature a sophisticated design crafted from 10K gold-filled metal. Measuring 2 inches in length and weighing a combined 52 grams, they exemplify the brand’s commitment to luxury and style. The substantial weight underscores their quality, making them a standout addition to any jewelry collection. While unsigned, their design and craftsmanship are indicative of Valentino’s 1980s aesthetic.",
-		category: "Earrings",
+		category: "earrings",
 		count: 0,
 		image: ["/images/e20.webp", "/images/e21.webp", "/images/e22.webp"],
 	},
@@ -25,7 +45,7 @@ const products = [
 		slug: "Baroque-Cascade-Clip-On-Earrings",
 		description:
 			"Dramatic and elegant, these 4-inch clip-on earrings feature cascading black and gold-tone beads with intricate detailing. The faceted black top stone complements the ornate, textured spheres and teardrop dangles, creating a luxurious baroque-inspired look. Weighing 38 grams combined, these statement earrings are perfect for evening wear or bold fashion styling.",
-		category: "Earrings",
+		category: "earrings",
 		count: 75,
 		image: [
 			"/images/e30.webp",
@@ -40,7 +60,7 @@ const products = [
 		slug: "Chr-Dior-Gold-Plated-Shell-Drop-Clip-On-Earrings",
 		description:
 			"Statement-making and sophisticated, these gold-plated clip-on earrings from Chr. Dior feature a striking double-shell drop design with fine ridged texture that catches the light beautifully. Signed on the clip backs, they measure 2.5 inches long and weigh 34g combined. A timeless piece for any collector or lover of bold, high-fashion accessories.",
-		category: "Earrings",
+		category: "earrings",
 		count: 5,
 		image: [
 			"/images/e40.webp",
@@ -55,7 +75,7 @@ const products = [
 		slug: "Chunky-Interlocked-Gold-Tone-Earrings",
 		description:
 			"Vintage pierced earrings featuring a bold, interlocked design in a rich gold-tone finish. The surface has a soft textured detail that enhances their sculptural shape. These earrings measure 1.2 inches long and weigh 26g combined. A striking and dimensional piece from the 1980s, perfect for adding vintage charm to any look.",
-		category: "Earrings",
+		category: "earrings",
 		count: 100,
 		image: ["/images/e50.webp", "/images/e51.webp", "/images/e52.webp"],
 	},
@@ -65,7 +85,7 @@ const products = [
 		slug: "Gold-Tone-Filigree-Clip-On-Earrings",
 		description:
 			"Elegant vintage clip-on earrings featuring a gold-tone finish with intricate filigree detailing. These statement earrings have a bold yet refined design, measuring 1.5 inches long and weighing 18 g combined. Their timeless aesthetic makes them a versatile addition to any vintage jewelry collection.",
-		category: "Earrings",
+		category: "earrings",
 		count: 60,
 		image: [
 			"/images/e60.webp",
@@ -80,7 +100,7 @@ const products = [
 		slug: "Gold-Tone-Statement-Chandelier-Clip-On-Earrings-1980s",
 		description:
 			"Striking vintage 1980s gold-tone clip-on chandelier earrings featuring an intricate cascading design. Measuring 4 inches long and weighing 20 grams combined, these earrings offer elegant movement and bold impact. The layered metal bars create a shimmering effect, making them a standout statement piece. Designed for comfort with secure clip-on backs, they embody the opulent and daring style of the 1980s.",
-		category: "Earrings",
+		category: "earrings",
 		count: 80,
 		image: [
 			"/images/e70.webp",
@@ -95,7 +115,7 @@ const products = [
 		slug: "Gold-Radiance-Rhinestone-Clip-On-Earrings",
 		description:
 			"These brand new deadstock clip-on earrings feature a radiant gold-tone floral design with a domed center paved in clear rhinestones. The sculpted petal edges catch the light beautifully, adding dimension and sparkle to any look. Measuring 1.3 inches and weighing 32g combined, these statement earrings are unworn vintage stock, offering the charm of the past with the freshness of new. 1080's",
-		category: "Earrings",
+		category: "earrings",
 		count: 35,
 		image: ["/images/e90.webp", "/images/e91.webp", "/images/e92.webp"],
 	},
@@ -105,7 +125,7 @@ const products = [
 		slug: "golden-swirl-hoop-earrings",
 		description:
 			"These vintage clip-on earrings from the 1980s feature an elegant, layered swirl design crafted in a polished gold-tone finish. Measuring 3 inches in length, 2 inches wide, and weighing 1.7 ounces, they are bold and sophisticated, perfect for adding a glamorous touch to any outfit. The intricate hoop-style construction with overlapping tubular designs makes them a timeless statement piece for vintage jewelry enthusiasts.",
-		category: "Earrings",
+		category: "earrings",
 		count: 25,
 		image: ["/images/e100.webp", "/images/e101.webp", "/images/e102.webp"],
 	},
@@ -115,7 +135,7 @@ const products = [
 		slug: "rajwadi-bollywood-gold-plated-dangle-earrings",
 		description:
 			"These exquisite Rajwadi Bollywood-inspired clip-on earrings feature a striking gold-plated design with cascading red glass beads and reflective accents. Measuring 4.2 inches long and weighing a combined 46 grams, they embody the grandeur of traditional Indian jewelry with a vintage touch. Perfect for making a bold statement.",
-		category: "Earrings",
+		category: "earrings",
 		count: 100,
 		image: ["/images/e200.webp", "/images/e201.webp", "/images/e202.webp"],
 	},
@@ -125,7 +145,7 @@ const products = [
 		slug: "gold-filled-clip-on-earrings-with-multicolor-accents",
 		description:
 			"Statement gold-tone clip-on earrings featuring a 10K gold-filled finish and an intricate leaf-inspired design. Adorned with blue, green, and red accent stones, these earrings add a bold touch to any look. Measuring 2.5 inches long and weighing 38 grams combined, they offer a substantial yet elegant presence. ",
-		category: "Earrings",
+		category: "earrings",
 		count: 70,
 		image: ["/images/e300.webp", "/images/e301.webp", "/images/e302.webp"],
 	},
@@ -135,7 +155,7 @@ const products = [
 		slug: "norma-jean-textured-silver-clip-on-earrings",
 		description:
 			"Silver-toned clip-on earrings with a layered ridged design and beaded texture. Signed Norma Jean. Measure 1.4 inches long and weigh 46g combined.",
-		category: "Earrings",
+		category: "earrings",
 		count: 15,
 		image: ["/images/e400.webp", "/images/e401.webp", "/images/e402.webp"],
 	},
@@ -145,7 +165,7 @@ const products = [
 		slug: "rhinestone-and-faux-pearl-clip-on-earrings",
 		description:
 			"Statement clip-on earrings from the 1980s featuring clear rhinestones and ivory-toned faux pearls arranged in a bold, cascading design. These dramatic earrings measure 3.3 inches long and weigh 44g combined.",
-		category: "Earrings",
+		category: "earrings",
 		count: 55,
 		image: ["/images/e500.webp", "/images/e501.webp", "/images/e502.webp"],
 	},
@@ -155,7 +175,7 @@ const products = [
 		slug: "portable-projector",
 		description:
 			"Bold clip-on earrings featuring an abstract sculpted design in matte gold, accented with a row of clear rhinestones. These statement pieces combine angular forms with soft sparkle for a unique modernist look. Clip-on backing.",
-		category: "Earrings",
+		category: "earrings",
 		count: 12,
 		image: ["/images/e600.webp", "/images/e601.webp", "/images/e602.webp"],
 	},
@@ -166,7 +186,7 @@ const products = [
 		slug: "sculpted-gold-clip-on-earrings-1980s",
 		description:
 			"Bold and sophisticated, these 1980s clip-on earrings feature a sculptural gold-toned design with a striking combination of smooth and textured finishes. Measuring 2 inches long and weighing 20 grams combined, they embody the dramatic yet refined aesthetic of the era. Perfect for adding a touch of vintage glamour to any outfit.",
-		category: "Earrings",
+		category: "earrings",
 		count: 60,
 		image: ["/images/e700.webp", "/images/e701.webp", "/images/e702.webp"],
 	},
@@ -176,7 +196,7 @@ const products = [
 		slug: "silver-toned-sculptural-clip-on-earrings",
 		description:
 			"Bold and modernist silver-toned clip-on earrings with a sculptural, layered design. Measuring 2 inches long and weighing 28 grams combined, these statement earrings reflect the sleek, avant-garde aesthetic of the 1980s. The polished finish and curved metal layers create a striking, dimensional effect, making them a standout addition to any vintage jewelry collection.",
-		category: "Earrings",
+		category: "earrings",
 		count: 45,
 		image: ["/images/e800.webp", "/images/e801.webp", "/images/e802.webp"],
 	},
@@ -186,7 +206,7 @@ const products = [
 		slug: "swirl-crest-earrings",
 		description:
 			"Clip-on earrings featuring a bold swirl design in polished silver-tone metal, embellished with clear rhinestones. These custom statement earrings measure 1.5 inches long and weigh 58g combined, offering a sculptural silhouette perfect for elevating any look.",
-		category: "Earrings",
+		category: "earrings",
 		count: 85,
 		image: ["/images/e900.webp", "/images/e901.webp", "/images/e902.webp"],
 	},
@@ -196,7 +216,7 @@ const products = [
 		slug: "10K-Gold-Statement-Clip-On-Earrings",
 		description:
 			"A striking pair of vintage 10K gold clip-on earrings, featuring a bold geometric design with a hammered texture and beaded edges. Measuring 2.5 inches long and weighing 44 grams combined, these statement earrings exude elegance and timeless charm. Year 1980s",
-		category: "Earrings",
+		category: "earrings",
 		count: 5,
 		image: ["/images/e10.webp", "/images/e11.webp", "/images/e12.webp"],
 	},
@@ -206,7 +226,7 @@ const products = [
 		slug: "golden-daisy-earrings",
 		description:
 			"Vintage 1980s clip-on earrings shaped like daisies with a textured gold-tone finish. Weighing 1.0 oz and measuring 1.8 inches, these charming floral earrings are perfect for adding a touch of retro elegance.",
-		category: "Earrings",
+		category: "earrings",
 		count: 50,
 		image: ["/images/e2000.webp", "/images/e2001.webp", "/images/e2002.webp"],
 	},
@@ -216,7 +236,7 @@ const products = [
 		slug: "sculpted-gold-clip-on-earrings-1980s",
 		description:
 			"Bold and sophisticated, these 1980s clip-on earrings feature a sculptural gold-toned design with a striking combination of smooth and textured finishes. Measuring 2 inches long and weighing 20 grams combined, they embody the dramatic yet refined aesthetic of the era. Perfect for adding a touch of vintage glamour to any outfit.",
-		category: "Earrings",
+		category: "earrings",
 		count: 50,
 		image: ["/images/e3000.webp", "/images/e3001.webp", "/images/e3002.webp"],
 	},
